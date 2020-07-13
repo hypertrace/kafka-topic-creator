a51dcdfbe8e387b92dc9dae6677b5261b300cd8b
Tim Mwangi <tim@traceable.ai>
2020-07-13 06:46:01 -0700

    chore: update to Apache 2.0 License

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,13 @@
 pluginManagement {
-    repositories {
-        mavenLocal()
-        gradlePluginPortal()
-        maven("https://dl.bintray.com/hypertrace/maven")
-    }
+  repositories {
+    mavenLocal()
+    gradlePluginPortal()
+    maven("https://dl.bintray.com/hypertrace/maven")
+  }
 }
 
 plugins {
-    id("org.hypertrace.version-settings") version "0.1.0"
+  id("org.hypertrace.version-settings") version "0.1.0"
 }
 
 rootProject.name = "kafka-topic-creator"
